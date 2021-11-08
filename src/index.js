07a544525e3fbbce8e71859e2232f72b9ed4e6e0
Hrydin <hrydin@bubuntu-lx.nixsolutions.com>
2021-11-08 19:04:20 +0200

    Deleted unnecessary files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import FilterableProductTable from "./thinking_in_react/FilterableProductTable";
 
