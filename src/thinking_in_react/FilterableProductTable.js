335c29ca8ee8f52fe9d629aa535bbf8ffab87895
Hrydin <hrydin@bubuntu-lx.nixsolutions.com>
2021-11-09 10:29:12 +0200

    Hooks. Introduction

--- src/thinking_in_react/FilterableProductTable.js
+++ src/thinking_in_react/FilterableProductTable.js
@@ -3,6 +3,19 @@ import ProductTable from "./ProductTable";
 import SearchBar from "./SearchBar";
 import "./../App.css";
 
+
+/*
+Example of input
+const products = [
+  {category: 'Sporting Goods', price: '49.99$', stocked: true, name: 'Football'},
+  {category: 'Sporting Goods', price: '9.99$', stocked: true, name: 'Baseball'},
+  {category: 'Sporting Goods', price: '29.99$', stocked: false, name: 'Basketball'},
+  {category: 'Electronics', price: '99.99$', stocked: true, name: 'iPod Touch'},
+  {category: 'Electronics', price: '399.99$', stocked: false, name: 'iPhone 5'},
+  {category: 'Electronics', price: '199.99$', stocked: true, name: 'Nexus 7'}
+];
+*/
+
 export default class FilterableProductTable extends React.Component {
 
   constructor(props) {
