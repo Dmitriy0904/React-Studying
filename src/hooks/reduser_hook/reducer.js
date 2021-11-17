84df6fe59131ae25e1464f7d7902b90f76cc526f
Dmitriy <dmitriygridin0904@gmail.com>
2021-11-17 13:17:12 +0300

    Hooks. UserReducer hook. Updated reducer function

--- src/hooks/reduser_hook/reducer.js
+++ src/hooks/reduser_hook/reducer.js
@@ -1,10 +1,16 @@
-function reducer(state, action){
-    switch(action.type){
-        case "increment": return { counter: state.counter + action.payload};
-        case "decrement": return { counter: state.counter - action.payload};
-        case "zeroing": return { counter: 0};
-        default: return state;
-    }
+import { INCREMENT, DECREMENT, ZEROING } from "./types";
+
+function reducer(state, action) {
+  switch (action.type) {
+    case INCREMENT:
+      return { counter: state.counter + action.payload };
+    case DECREMENT:
+      return { counter: state.counter - action.payload };
+    case ZEROING:
+      return { counter: 0 };
+    default:
+      return { ...state };
+  }
 }
 
 export default reducer;
