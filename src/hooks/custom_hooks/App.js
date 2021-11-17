c032a14d71a1aaad7e0ee5015095745af6e981cd
Dmitriy <dmitriygridin0904@gmail.com>
2021-11-17 13:52:20 +0300

    Custom hooks. UseToggle custom hook

--- src/hooks/custom_hooks/App.js
+++ src/hooks/custom_hooks/App.js
@@ -2,12 +2,14 @@ import React, {useState} from "react";
 import useCounter from "./useCounter";
 import useInput from "./useInput";
 import useOne from "./useOne";
+import useToggle from "./useToggle";
 
 function App(){
 
-    useOne(() => {
-        console.log("Called 1 time");
-    });
+    //Example of useOne custom hook
+    // useOne(() => {
+    //     console.log("Called 1 time");
+    // });
 
 
     //example of useInput custom hook
@@ -23,16 +25,25 @@ function App(){
 
 
     //example of useCounter custom hook
-    const {counter, count} = useCounter();
+    // const {counter, count} = useCounter();
+
+    // return (
+    //     <div>
+    //         <h3>Clicked {counter} times</h3>
+    //         <button onClick={count}>Press me!</button>
+    //     </div>
+    // );
+
+
+    //example of useToggle custom hook
+    const [isVisible, toggleVisible] = useToggle(true);
 
     return (
-        <div>
-            <h3>Clicked {counter} times</h3>
-            <button onClick={count}>Press me!</button>
-        </div>
+      <div>
+        {isVisible && <h2>Hello world</h2>}
+        <button onClick={toggleVisible}>Toggle</button>
+      </div>
     );
-
-
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./hooks/reduser_hook/App.js";
+import App from "./hooks/custom_hooks/App"
 import reportWebVitals from "./reportWebVitals";
 
 
